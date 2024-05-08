726adccd50f346c8c75e56b05439b554b0678a49
nexoc <davletshinm@list.ru>
2024-05-08 21:04:08 +0200

    add MovieDto

--- src/main/java/at/davl/movie/repository/MovieRepository.java
+++ src/main/java/at/davl/movie/repository/MovieRepository.java
@@ -3,6 +3,8 @@ package at.davl.movie.repository;
 import at.davl.movie.models.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
 
 }
