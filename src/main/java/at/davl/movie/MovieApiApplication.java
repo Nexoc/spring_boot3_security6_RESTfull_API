83cf60a69fffef5cbea35fc2f51b7d58414d2564
nexoc <davletshinm@list.ru>
2024-05-08 21:17:43 +0200

    MovieDto is filled

--- src/main/java/at/davl/movie/MovieApiApplication.java
+++ src/main/java/at/davl/movie/MovieApiApplication.java
@@ -3,7 +3,8 @@ package at.davl.movie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+// 3. Uploading and Serving file at Backend | Spring Boot
+// 6.27
 
 @SpringBootApplication
 public class MovieApiApplication {
